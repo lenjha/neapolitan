8b3e86b531a4f1c79f15bc34b9dfa6312d23b2ad
Lena Ha <vermilionha@gmail.com>
2018-05-02 17:01:46 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">h a h a !</h1>
+          <div className="nav">
+            <ul>
+              <li className="navlink">r é s u m é</li>
+              <li className="navlink">g a l l e r y</li>
+              <li className="navlink">a b o u t</li>
+            </ul>
+          </div>
+        </header>
+        <p className="App-intro">
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
